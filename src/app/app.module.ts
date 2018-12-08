8b3d9e66fcaebec62509b039855f27d9be61a81a
Andrei Kaliachka <akaliachka@gmail.com>
2018-12-08 15:38:13 +0300

    beggining with ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {environment} from '../environments/environment.prod';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtools, StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {reducers} from './shared/store';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [],
   bootstrap: [AppComponent]
